15ab2b64c9b7c51a684c57de91a8d23d05c524bf
Russell Pena <russellpena@Russells-MacBook-Pro.local>
2017-07-22 08:09:34 -0400

    added nearby gym locations

--- Project/public/js/script.js
+++ Project/public/js/script.js
@@ -1,9 +1,44 @@
 $(document).ready(function () {
 
+
+    var myLatLng = new google.maps.LatLng(28.521381, -81.466026);
     var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
+        center: myLatLng,
         scrollwheel: false,
-        zoom: 8
+        zoom: 12
     });
+
+    //marker
+    function createMarker(latlng,icn,name) {
+        var marker = new google.maps.Marker({
+            position: latlng,
+            map: map,
+            icon: icn,
+            title: name
+        });
+    }
+
+    var request = {
+        location: myLatLng,
+        radius: '10000',
+        type: ['gym']
+    };
+
+
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+
+    function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                var place = results[i];
+                latlng = place.geometry.location;
+                icn = place.icon;
+                name = place.name;
+                createMarker(latlng,icn,name);
+            }
+        }
+    }
+
 
 });
